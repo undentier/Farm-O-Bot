1c0bcb81aae515cdb053df5dcfd287f95fbe101d
Antoine <a.leroux@rubika-edu.com>
2022-05-03 22:13:48 +0200

    Controller like AT-ST done

--- Farm O Bot/Assets/Lab/Antoine/Scripts/MechaControllerMovement.cs
+++ Farm O Bot/Assets/Lab/Antoine/Scripts/MechaControllerMovement.cs
@@ -9,11 +9,14 @@ public class MechaControllerMovement : MonoBehaviour
     [Header("Movement")]
     public float speed;
     //public float rotationSpeedLegs;
+    public bool legMoveWithChestRotation = false;
     public float turnTime;
+    private bool turnDownBody = false;
+    private float targetAngleChest = 0;
     private float turnVelocity;
     public Transform legs;
     private float legsRotationX;
-
+    private float legsBaseAngle;
     [Header("Aim")]
     public float rotationSpeedChest;
     public bool clampChestRotationHorizontal = false;
@@ -23,6 +26,7 @@ public class MechaControllerMovement : MonoBehaviour
     private float chestRotationX;
     private float chestRotationY;
     public Transform chest;
+
 
     private Vector2 inputMovement;
     private Vector2 inputLook;
@@ -38,13 +42,15 @@ public class MechaControllerMovement : MonoBehaviour
         mechaAnimationScript = GetComponent<MechaAnimation>();
         playerActions = new DefaultInputActions();
         playerActions.Player.Enable();
+
+        legsBaseAngle = (legs.eulerAngles.y - 180);
     }
 
     private void Update()
     {
         ReadInput();
 
-        RotateMechaDownBody();
+        if(legMoveWithChestRotation) RotateMechaDownBody();
         RotateMechaUpBody();
         MoveMecha();
     }
@@ -60,22 +66,40 @@ public class MechaControllerMovement : MonoBehaviour
         //legsRotationX += inputMovement.x * rotationSpeedLegs * Time.deltaTime;
 
         //legs.rotation = Quaternion.Euler(legs.rotation.eulerAngles.x, legsRotationX + 180f, legs.rotation.eulerAngles.z);
+
+        if (chestRotationX >= clampAngleHorizontal.y || chestRotationX <= clampAngleHorizontal.x)
+        {
+            targetAngleChest = chest.eulerAngles.y -180;
+            chestRotationX = 0;
+            //legs.rotation = Quaternion.Euler(legs.rotation.eulerAngles.x, targetAngleChest, legs.rotation.eulerAngles.z);
+            legsBaseAngle = (targetAngleChest - 180);
+            turnDownBody = true;
+        }
+
+        if (turnDownBody)
+        {
+            float angle = Mathf.SmoothDampAngle(legs.eulerAngles.y, targetAngleChest, ref turnVelocity, turnTime);
+            legs.rotation = Quaternion.Euler(legs.rotation.eulerAngles.x, angle, legs.rotation.eulerAngles.z);
+        }
     }
 
     private void RotateMechaUpBody()
     {
         Vector3 lookDirection = new Vector2(inputLook.x, inputLook.y).normalized;
 
-        chestRotationX += lookDirection.x * rotationSpeedChest * Time.deltaTime;
-        chestRotationY += -lookDirection.y * rotationSpeedChest * Time.deltaTime;
+        if (lookDirection.magnitude >= 0.2f)
+        {
+            chestRotationX += lookDirection.x * rotationSpeedChest * Time.deltaTime;
+            chestRotationY += -lookDirection.y * rotationSpeedChest * Time.deltaTime;
 
-        if (clampChestRotationHorizontal) chestRotationX = Mathf.Clamp(chestRotationX, clampAngleHorizontal.x, clampAngleHorizontal.y);
-        if (clampChestRotationVertical) chestRotationY = Mathf.Clamp(chestRotationY, clampAngleVertical.x, clampAngleVertical.y);
+            if (clampChestRotationHorizontal) { chestRotationX = Mathf.Clamp(chestRotationX, clampAngleHorizontal.x, clampAngleHorizontal.y); ClampAngle(chestRotationX, clampAngleHorizontal.x, clampAngleHorizontal.y); }
+            if (clampChestRotationVertical) { chestRotationY = Mathf.Clamp(chestRotationY, clampAngleVertical.x, clampAngleVertical.y); ClampAngle(chestRotationY, clampAngleVertical.x, clampAngleVertical.y); }
 
-        //chest.rotation = Quaternion.Euler(chestRotationY, legsRotationX + chestRotationX, 0);
-        chest.rotation = Quaternion.Euler(chestRotationY, chestRotationX, 0);
+            //chest.rotation = Quaternion.Euler(chestRotationY, legsRotationX + chestRotationX, 0);
+            chest.rotation = Quaternion.Euler(chestRotationY, legsBaseAngle + chestRotationX, 0);
+        }
     }
-
+    
     private void MoveMecha()
     {
         /*Vector3 movement = -chest.forward * -inputMovement.y;
@@ -101,9 +125,27 @@ public class MechaControllerMovement : MonoBehaviour
 
             //Mecha movement
             //Vector3 directionForward = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            /*targetAngleChest = chest.eulerAngles.y - 180;
+            turnDownBody = true;*/
+
+            //legs.rotation = Quaternion.Euler(legs.rotation.eulerAngles.x, chest.eulerAngles.y - 180, legs.rotation.eulerAngles.z);
             rb.MovePosition(rb.position + movementDirection * speed * Time.deltaTime);
             mechaAnimationScript.WalkAnimation(true);
         }
         else mechaAnimationScript.WalkAnimation(false);
+
+        if (turnDownBody)
+        {
+            float angle = Mathf.SmoothDampAngle(legs.eulerAngles.y, targetAngleChest, ref turnVelocity, turnTime);
+            legs.rotation = Quaternion.Euler(legs.rotation.eulerAngles.x, angle, legs.rotation.eulerAngles.z);
+        }
+    }
+
+    private float ClampAngle(float angle, float from, float to)
+    {
+        if (angle < 0f) angle = 360 + angle;
+        if (angle > 180f) return Mathf.Max(angle, 360 + from);
+        return Mathf.Min(angle, to);
     }
 }
