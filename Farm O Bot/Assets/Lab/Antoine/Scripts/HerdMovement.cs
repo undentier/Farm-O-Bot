c9f82ce88396b6278e401b9cb5420bbfe17e5eb8
Antoine <a.leroux@rubika-edu.com>
2022-05-10 21:58:51 +0200

    start movement behavior herd to nearest zone

--- Farm O Bot/Assets/Lab/Antoine/Scripts/HerdMovement.cs
+++ Farm O Bot/Assets/Lab/Antoine/Scripts/HerdMovement.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class HerdMovement : MonoBehaviour
 {
+    [Header("Bison")]
     public GameObject bison;
+    public float bisonMoveFrequence;
+
+    [Header("Herd")]
+    public Transform zones;
+    public Transform[] zonesPosition;
+    public float[] zonesDistance;
+    public float[] ZonesDistanceOrdered;
+    public Transform[] nearestZones;
     public int numberBisons;
     public float herdRadius;
-    public float bisonMoveFrequence;
     public float herdMoveFrequence;
+    private float herdMoveTimer = 0;
 
     [Header("Debug")]
     public bool debugHerdRadius = false;
@@ -19,6 +29,21 @@ public class HerdMovement : MonoBehaviour
         {
             Instantiate(bison, transform.position + new Vector3(Random.insideUnitSphere.x * herdRadius, transform.position.y, Random.insideUnitSphere.z * herdRadius), bison.transform.rotation, transform);
         }
+
+        zonesPosition = new Transform[zones.childCount];
+        for (int i = 0; i < zones.childCount; i++)
+        {
+            zonesPosition[i] = zones.GetChild(i);
+        }
+
+        zonesDistance = new float[zones.childCount];
+        RefreshZonesDistance();
+
+        ZonesDistanceOrdered = zonesDistance;
+        
+        nearestZones = new Transform[2];
+
+        SortNearestZones();
     }
 
     private void OnDrawGizmos()
@@ -27,6 +52,25 @@ public class HerdMovement : MonoBehaviour
         {
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(transform.position, herdRadius);
+        }
+    }
+
+    private void RefreshZonesDistance()
+    {
+        for (int i = 0; i < zonesPosition.Length; i++)
+        {
+            zonesDistance[i] = (Vector3.Distance(zonesPosition[i].position, transform.position));
+        }
+    }
+
+    private void SortNearestZones()
+    {
+        ZonesDistanceOrdered = ZonesDistanceOrdered.OrderBy(x => x).ToArray();
+
+        for (int i = 0; i < nearestZones.Length; i++)
+        {
+            int index = System.Array.IndexOf(zonesDistance, ZonesDistanceOrdered[i+1]);
+            nearestZones[i] = zonesPosition[index];
         }
     }
 }
