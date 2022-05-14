6981360dd26d27051be15a2b9c2c4a11528f3390
Antoine <a.leroux@rubika-edu.com>
2022-05-14 02:24:24 +0200

    build system for turret done

--- Farm O Bot/Assets/Lab/Antoine/Scripts/BuildSystem.cs
+++ Farm O Bot/Assets/Lab/Antoine/Scripts/BuildSystem.cs
@@ -5,10 +5,20 @@ using UnityEngine.InputSystem;
 
 public class BuildSystem : MonoBehaviour
 {
+    [Header("Turrets")]
     public int maxNumberTurretsPlaced;
-    private int currentNumberTurrets;
-    private bool turretVisualisation = false;
+    [HideInInspector] public List<GameObject> currentTurretsNumber;
+    private bool turretIsPreview = false;
+    public float maxDistanceBuild;
+    private float distanceAimPoint;
 
+    public GameObject turret;
+    public GameObject turretPreview;
+    private GameObject turretPreviewInstance;
+
+    [Space]
+    public Transform aimPosition;
+    public Transform chest;
     private PlayerInput playerInputScript;
     
     private void Start()
@@ -25,7 +35,10 @@ public class BuildSystem : MonoBehaviour
     {
         if (context.started)
         {
-            turretVisualisation = !turretVisualisation;
+            turretIsPreview = !turretIsPreview;
+
+            if(turretIsPreview) SelectTurretPreview(true);
+            else SelectTurretPreview(false);
         }
     }
 
@@ -37,12 +50,66 @@ public class BuildSystem : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        distanceAimPoint = Vector3.Distance(aimPosition.position, transform.position);
+
+        UpdateTurretPreview();
+    }
+
+    private void SelectTurretPreview(bool enable)
+    {
+        switch (enable)
+        {
+            case true:
+                turretPreviewInstance = Instantiate(turretPreview, aimPosition.position, Quaternion.identity);
+                break;
+            case false:
+                Destroy(turretPreviewInstance);
+                break;
+        }
+    }
+
+    private void UpdateTurretPreview()
+    {
+        if (turretIsPreview)
+        {
+            if (distanceAimPoint < maxDistanceBuild)
+            {
+                turretPreviewInstance.SetActive(true);
+                turretPreviewInstance.transform.position = aimPosition.position;
+                turretPreviewInstance.transform.rotation = Quaternion.Euler(0, chest.rotation.eulerAngles.y, 0);
+            }
+            else
+            {
+                turretPreviewInstance.SetActive(false);
+            }
+        }
+    }
+
     private void PlaceTurretInWorld()
     {
-        if (turretVisualisation && currentNumberTurrets < maxNumberTurretsPlaced)
+        if (turretIsPreview && distanceAimPoint < maxDistanceBuild)
         {
+            if (currentTurretsNumber.Count < maxNumberTurretsPlaced)
+            {
+                GameObject turretInstance = Instantiate(turret, turretPreviewInstance.transform.position, turretPreviewInstance.transform.rotation);
+                currentTurretsNumber.Add(turretInstance);
+            }
+            else
+            {
+                for (int i = 0; i < currentTurretsNumber.Count; i++)
+                {
+                    Destroy(currentTurretsNumber[i]);
+                }
+                currentTurretsNumber.Clear();
+
+                GameObject turretInstance = Instantiate(turret, turretPreviewInstance.transform.position, turretPreviewInstance.transform.rotation);
+                currentTurretsNumber.Add(turretInstance);
+            }
             
-            currentNumberTurrets++;
+            Destroy(turretPreviewInstance);
+            turretIsPreview = false;
         }
     }
 }
